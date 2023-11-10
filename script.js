540a41785b34aeffe61c9e9a9223fcf917046d32
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-11-10 19:18:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+	if(date1 == date2) {return 0};
+	let arr1 = date1.split('-');
+	let arr2 = date2.split('-');
+	let days = 0;
+	for(let i = 0; i < arr1.length; i++){
+		if(arr1[i] > arr2[i]){
+			return -1;
+		}
+	}
+
+	let y1 = New Date(date1);
+	let y2 = New Date(date2);
+	let diff = y2 - y1;
+
+	return Math.floor(diff/(1000*60*60*24));
 };
 
 // Do not change the code below.
