612a937fed9b859a8233b6a0bb44ebb4f96afd98
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-11-10 19:26:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ var dateDiffInDays = function (date1, date2) {
 		}
 	}
 
-	let y1 = New Date(date1);
-	let y2 = New Date(date2);
+	let y1 = new Date(date1).getTime();
+	let y2 = new Date(date2).getTime();
 	let diff = y2 - y1;
 
 	return Math.floor(diff/(1000*60*60*24));
